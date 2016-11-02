5c41ea6ba825c7bb284ac39f9385680be958c890
Peli de Halleux <jhalleux@microsoft.com>
2016-11-02 16:57:14 -0700

    adding tests

--- tests.ts
+++ tests.ts
@@ -1,1 +1,4 @@
 // tests go here; this will not be compiled when this package is used as a library
+files.appendString("output.txt", "appendstring");
+files.appendNumber("output.txt", 42);
+files.appendLine("output.txt", "appendline");
