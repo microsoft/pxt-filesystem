b019f4f05a9a4bb3e42e036a55811f56cedaf4fd
Peli de Halleux <jhalleux@microsoft.com>
2017-03-09 08:30:17 -0800

    updated shims

--- shims.d.ts
+++ shims.d.ts
@@ -12,16 +12,16 @@ declare namespace files {
      * @param filename file name, eg: "output.txt"
      * @param text the string to append to the end of the file
      */
-    //% blockId="files_append_line" block="file %filename|append string %text"
+    //% blockId="files_append_line" block="file %filename|append line %text"
     //% blockExternalInputs=1 weight=90 blockGap=8 shim=files::appendLine
     function appendLine(filename: string, text: string): void;
 
     /**
-     * Appends text and a new line to a file
+     * Appends text to a file
      * @param filename file name, eg: "output.txt"
      * @param text the string to append to the end of the file
      */
-    //% blockId="fs_append_string" block="file %filename|append line %text"
+    //% blockId="fs_append_string" block="file %filename|append string %text"
     //% blockExternalInputs=1 weight=86 blockGap=8 shim=files::appendString
     function appendString(filename: string, text: string): void;
 
