a778b347d77c45e654b7e76eeb0f409407004899
Peli de Halleux <jhalleux@microsoft.com>
2017-03-27 16:45:29 -0700

    updated generated files

--- enums.d.ts
+++ enums.d.ts
@@ -1,4 +1,32 @@
 // Auto-generated. Do not edit.
+
+
+    /**
+     * File seek offset modifier
+     */
+
+    declare enum FileSystemOpenFlags {
+    //% block=read
+    Read = 0x01,  // MB_READ
+    //% block=write
+    Write = 0x02,  // MB_WRITE
+    //% block=end
+    Create = 0x04,  // MB_CREAT
+    }
+
+
+    /**
+     * File seek offset modifier
+     */
+
+    declare enum FileSystemSeekFlags {
+    //% block=set
+    Set = 0x01,  // MB_SEEK_SET
+    //% block=current
+    Current = 0x04,  // MB_SEEK_CUR
+    //% block=end
+    End = 0x02,  // MB_SEEK_END
+    }
 declare namespace files {
 }
 
