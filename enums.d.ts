bc99ec78e42794c450a5c628aa2fd36d001e8227
Peli de Halleux <jhalleux@microsoft.com>
2017-04-05 13:44:32 -0700

    missing changes

--- enums.d.ts
+++ enums.d.ts
@@ -5,20 +5,6 @@
      * File seek offset modifier
      */
 
-    declare enum FileSystemOpenFlags {
-    //% block=read
-    Read = 0x01,  // MB_READ
-    //% block=write
-    Write = 0x02,  // MB_WRITE
-    //% block=end
-    Create = 0x04,  // MB_CREAT
-    }
-
-
-    /**
-     * File seek offset modifier
-     */
-
     declare enum FileSystemSeekFlags {
     //% block=set
     Set = 0x01,  // MB_SEEK_SET
